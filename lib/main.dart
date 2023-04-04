1f991c23ae0fc0475db9785a69dbfdbf51fd0eae
tinghaolai <analth355@gmail.com>
2023-04-04 22:09:29 +0800

    App loading and fake login check

--- lib/main.dart
+++ lib/main.dart
@@ -5,16 +5,55 @@ void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  MyAppState createState() => MyAppState();
+}
+
+class MyAppState extends State<MyApp> {
+  bool _needCheckLogin = true;
+  bool _isLogin = false;
+
+  Future<bool> _checkLogin() async {
+    await Future.delayed(const Duration(seconds: 2));
+    return false;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _checkLogin().then((value) {
+      setState(() {
+        _needCheckLogin = false;
+        _isLogin = value;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    if (_needCheckLogin) {
+      return const MaterialApp(
+        home: Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
+        ),
+      );
+    }
+    if (_isLogin) {
+      return const MaterialApp(
+        home: MyHomePage(title: 'Flutter Demo Home Page'),
+      );
+    }
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Not Login'),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
