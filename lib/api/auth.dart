10bac583f99c0efc501182a40869ccbe58b4641d
tinghaolai <analth355@gmail.com>
2023-04-19 19:49:24 +0800

    Api rout modify

--- lib/api/auth.dart
+++ lib/api/auth.dart
@@ -3,12 +3,13 @@ import 'dart:convert';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
 
+import '../constants/api_constants.dart';
+
 class AuthApi {
   static final String? baseAPIUrl = dotenv.env['BASE_API_URL'];
 
   static Future<Map<String, dynamic>> login(String email, String password) async {
-    const String path = '/api/login';
-    final Uri uri = Uri.parse('$baseAPIUrl$path');
+    final Uri uri = Uri.parse('${APIConstants.apiPath}/login');
 
     final response = await http.post(uri, body: {
       'email': email,
@@ -23,9 +24,7 @@ class AuthApi {
   }
 
   static Future<http.Response> register(String username, String password, String email) {
-    const String path = '/api/register';
-
-    final Uri uri = Uri.parse('$baseAPIUrl$path');
+    final Uri uri = Uri.parse('${APIConstants.apiPath}/register');
     return http.post(uri, body: {
       'username': username,
       'password': password,
