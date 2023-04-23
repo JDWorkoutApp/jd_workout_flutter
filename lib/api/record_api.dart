4a314fb4ff90f2da72af391fe6feb15b6e4a0638
tinghaola <analth355@gmail.com>
2023-04-23 16:45:48 +0800

    Api format using class

--- lib/api/record_api.dart
+++ lib/api/record_api.dart
@@ -1,5 +1,101 @@
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+class TrainingData {
+  final int total;
+  final List<Training> trainings;
+
+  TrainingData({
+    required this.total,
+    required this.trainings,
+  });
+
+  factory TrainingData.fromJson(Map<String, dynamic> json) {
+    return TrainingData(
+      total: json['total'],
+      trainings: List<Training>.from(
+        json['data'].map((training) => Training.fromJson(training)),
+      ),
+    );
+  }
+}
+
+class Training {
+  final String date;
+  final String start;
+  final String end;
+  final List<Equipment> equipments;
+
+  Training({
+    required this.date,
+    required this.start,
+    required this.end,
+    required this.equipments,
+  });
+
+  factory Training.fromJson(Map<String, dynamic> json) {
+    return Training(
+      date: json['date'],
+      start: json['start'],
+      end: json['end'],
+      equipments: List<Equipment>.from(
+        json['equips'].map((equipment) => Equipment.fromJson(equipment)),
+      ),
+    );
+  }
+}
+
+class Equipment {
+  final int id;
+  final String name;
+  final String note;
+  final List<Record> records;
+
+  Equipment({
+    required this.id,
+    required this.name,
+    required this.note,
+    required this.records,
+  });
+
+  factory Equipment.fromJson(Map<String, dynamic> json) {
+    return Equipment(
+      id: json['id'],
+      name: json['name'],
+      note: json['note'],
+      records: List<Record>.from(
+        json['records'].map((record) => Record.fromJson(record)),
+      ),
+    );
+  }
+}
+
+class Record {
+  final int id;
+  final int weight;
+  final int reps;
+  final int sets;
+  final List<String> notes;
+
+  Record({
+    required this.id,
+    required this.weight,
+    required this.reps,
+    required this.sets,
+    required this.notes,
+  });
+
+  factory Record.fromJson(Map<String, dynamic> json) {
+    return Record(
+      id: json['id'],
+      weight: json['weight'],
+      reps: json['reps'],
+      sets: json['sets'],
+      notes: List<String>.from(json['notes']),
+    );
+  }
+}
+
+
 class RecordApi {
   static final String? baseAPIUrl = dotenv.env['BASE_API_URL'];
 
@@ -17,8 +113,8 @@ class RecordApi {
     return true;
   }
 
-  static Future<Map<String, dynamic>> get(int page) async {
-    return {
+  static Future<TrainingData> get(int page) async {
+    Map<String, dynamic> json = {
       "total": 55,
       "data": [
         {
@@ -86,5 +182,9 @@ class RecordApi {
         },
       ]
     };
+
+    TrainingData trainingData = TrainingData.fromJson(json);
+
+    return trainingData;
   }
 }
