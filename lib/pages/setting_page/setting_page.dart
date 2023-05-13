2779de097785a5a8b9678d6edbc51456580235fe
tinghaolai <analth355@gmail.com>
2023-05-13 09:41:28 +0800

    Setting page style refactor

--- lib/pages/setting_page/setting_page.dart
+++ lib/pages/setting_page/setting_page.dart
@@ -5,24 +5,45 @@ import 'package:workout_app/pages/login_page/login_page.dart';
 class SettingPage extends StatelessWidget {
   const SettingPage({Key? key});
 
+  void _logout(BuildContext context) async {
+    SharedPreferences.getInstance().then((prefs) {
+      prefs.remove("jwtToken");
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const LoginPage()));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            SharedPreferences.getInstance().then((prefs) {
-              prefs.remove("jwtToken");
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(
-                    builder: (context) => const LoginPage()
-              ));
-            });
-          },
-          child: const Text("Log out"),
-        ),
+        body: Center(
+            child: Container(
+      constraints:
+          BoxConstraints(maxWidth: MediaQuery.of(context).size.width * 0.5),
+      child: Expanded(
+        child: Container(
+            height: 70,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                minimumSize: const Size.fromHeight(10),
+                backgroundColor: Theme.of(context).cardColor,
+                foregroundColor: Theme.of(context).hintColor,
+                side:
+                    BorderSide(color: Theme.of(context).shadowColor, width: 3),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+              ),
+              onPressed: () => _logout(context),
+              child: const Text(
+                'LOGOUT',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 40,
+                ),
+              ),
+            )),
       ),
-    );
+    )));
   }
 }
