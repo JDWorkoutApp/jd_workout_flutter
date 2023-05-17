a21eae30ebe674a426906f9904e5e258e57f4572
tinghaolai <analth355@gmail.com>
2023-05-17 20:09:30 +0800

    Get equip api modify

--- lib/api/equip_api.dart
+++ lib/api/equip_api.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:workout_app/models/equip_summary_list_model.dart';
 import 'package:workout_app/utils/api_client.dart';
 
 class EquipApi {
@@ -27,102 +28,19 @@ class EquipApi {
     return true;
   }
 
-  static Future<Map<String, dynamic>> get(int page) async {
-    return {
-      "total": 55,
-      "data": [
-        {
-          "id": 11,
-          "name": "Bench Press",
-          "note": "Note for bench press",
-          "weights": [200, 1.0, 2.1, 3.3, 4.4, 5.0, 6],
-        },
-        {
-          "id": 12,
-          "name": "Pull equip",
-          "note": "Note for Pull equip",
-          "weights": [200, 1.0, 2.1, 3.3, 4.4, 5.0, 6],
-        },
-        {
-          "id": 13,
-          "name": "Leg equip",
-          "note": "Note for Leg equip",
-          "weights": [200, 1.0, 2.1, 3.3, 4.4, 5.0, 6],
-        },
-        {
-          "id": 14,
-          "name": "equip....",
-          "note": ".............",
-          "weights": [200, 1.0, 2.1, 3.3, 4.4, 5.0, 6],
-        },
-        {
-          "id": 15,
-          "name": "test equip",
-          "note": "test note",
-          "weights": [200, 1.0, 2.1, 3.3, 4.4, 5.0, 6],
-        },
-        {
-          "id": 16,
-          "name": "test equip",
-          "note": "test note",
-          "weights": [200, 1.0, 2.1, 3.3, 4.4, 5.0, 6],
-        },
-        {
-          "id": 17,
-          "name": "test equip",
-          "note": "test note",
-          "weights": [200, 1.0, 2.1, 3.3, 4.4, 5.0, 6],
-        },
-        {
-          "id": 18,
-          "name": "test equip",
-          "note": "test note",
-          "weights": [200, 1.0, 2.1, 3.3, 4.4, 5.0, 6],
-        },
-        {
-          "id": 19,
-          "name": "test equip",
-          "note": "test note",
-          "weights": [200, 1.0, 2.1, 3.3, 4.4, 5.0, 6],
-        },
-        {
-          "id": 20,
-          "name": "test equip",
-          "note": "test note",
-          "weights": [200, 1.0, 2.1, 3.3, 4.4, 5.0, 6],
-        },
-        {
-          "id": 21,
-          "name": "test equip",
-          "note": "test note",
-          "weights": [200, 1.0, 2.1, 3.3, 4.4, 5.0, 6],
-        },
-        {
-          "id": 22,
-          "name": "test equip",
-          "note": "test note",
-          "weights": [200, 1.0, 2.1, 3.3, 4.4, 5.0, 6],
-        },
-        {
-          "id": 23,
-          "name": "test equip",
-          "note": "test note",
-          "weights": [200, 1.0, 2.1, 3.3, 4.4, 5.0, 6],
-        },
-        {
-          "id": 24,
-          "name": "test equip",
-          "note": "test note",
-          "weights": [200, 1.0, 2.1, 3.3, 4.4, 5.0, 6],
-        },
-        {
-          "id": 25,
-          "name": "test equip",
-          "note": "test note",
-          "weights": [200, 1.0, 2.1, 3.3, 4.4, 5.0, 6],
-        },
-      ]
-    };
+  static Future<EquipSummaryListModel> get(int page) async {
+    final response = await ApiClient().get(ApiClient.getUri('/equip/?perPage=10&currentPage=$page'));
+
+    if (response.statusCode != 200) {
+      throw Exception('Failed to load equips');
+    }
+
+    var body = response.body;
+    Map<String, dynamic> data = jsonDecode(body);
+
+    EquipSummaryListModel list = EquipSummaryListModel.fromJson(data);
+
+    return list;
   }
 
   static Future<bool> putWeight(int id, List<double> weights) async {
