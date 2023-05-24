e73b0a21905e5fb416d0b92abb70bad30bbbfa30
tinghaolai <analth355@gmail.com>
2023-05-24 19:15:16 +0800

    Logo text modify

--- lib/pages/login_page/component/logo_text.dart
+++ lib/pages/login_page/component/logo_text.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:vizor/components/atoms/text_decoding/text_decoding.dart';
 
 class LogoText extends StatelessWidget {
   const LogoText({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text(
-      'GOVELFIT',
-      style: TextStyle(
-        fontSize: 45,
-        fontWeight: FontWeight.bold,
-        fontStyle: FontStyle.italic,
-        color: Theme.of(context).secondaryHeaderColor,
-      ),
-    );
+    return TextDecoding('GOVELFIT',
+        style: TextStyle(
+          fontSize: 45,
+          fontWeight: FontWeight.bold,
+          fontStyle: FontStyle.italic,
+          color: Theme.of(context).secondaryHeaderColor,
+        ));
   }
 }
