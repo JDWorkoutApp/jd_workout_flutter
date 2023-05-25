97acac2baada2a6e0bd5e13677f2da5c067429b6
tinghaolai <analth355@gmail.com>
2023-05-26 01:22:00 +0800

    Exercise page refactor

---
+++ lib/list/exercise_list_sliver.dart
@@ -0,0 +1,197 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:vizor/components/atoms/vizor_frame.dart';
+import 'package:workout_app/api/training_record_api.dart';
+import 'package:workout_app/models/training_model.dart';
+
+class ExerciseListSliver extends StatefulWidget {
+  const ExerciseListSliver({Key? key}) : super(key: key);
+
+  @override
+  _ExerciseListSliverState createState() => _ExerciseListSliverState();
+}
+
+class _ExerciseListSliverState extends State<ExerciseListSliver> {
+  final ScrollController _scrollController = ScrollController();
+  List<TrainingModel> _items = [];
+  int _page = 1;
+  bool _isLoading = false;
+  final _equipImageList = [
+    'assets/images/back-training.jpg',
+    'assets/images/leg-training.png',
+    'assets/images/chest-training.png',
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _scrollController.addListener(_onScroll);
+
+    _loadMoreItems();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  void _onScroll() {
+    if (_scrollController.position.pixels ==
+        _scrollController.position.maxScrollExtent) {
+      _loadMoreItems();
+    }
+  }
+
+  Future<void> _loadMoreItems() async {
+    if (_isLoading) {
+      return;
+    }
+
+    setState(() {
+      _isLoading = true;
+    });
+
+    var result = await TrainingRecordApi.get(_page);
+
+    setState(() {
+      _items.addAll(result.trainings);
+      _page++;
+      _isLoading = false;
+    });
+  }
+
+  dateFormat(String dateTimeString) {
+    DateTime dateTime = DateTime.parse(dateTimeString);
+    return dateTime.hour.toString().padLeft(2, '0') +
+        ':' +
+        dateTime.minute.toString().padLeft(2, '0');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        (context, index) {
+          if (index == _items.length) {
+            return _buildProgressIndicator();
+          } else {
+            TrainingModel training = _items[index];
+
+            return Dismissible(
+              key: Key(index.toString()),
+              child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: VizorFrame(
+                    child: Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Column(
+                        children: [
+                          Row(children: [
+                            Expanded(
+                                flex: 3,
+                                child: Align(
+                                  alignment: Alignment.topLeft,
+                                  child: Text(
+                                    training.date,
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                )),
+                            Expanded(
+                                flex: 4,
+                                child: Text(
+                                  dateFormat(training.start) +
+                                      " ~ " +
+                                      dateFormat(training.end),
+                                  style: TextStyle(
+                                    color:
+                                        Theme.of(context).secondaryHeaderColor,
+                                  ),
+                                )),
+                            Expanded(flex: 4, child: Container())
+                          ]),
+                          Padding(
+                              padding: EdgeInsets.only(top: 10, bottom: 10),
+                              child: Divider(
+                                color: Colors.grey,
+                                height: 1,
+                                thickness: 2,
+                              )),
+                          ListView.builder(
+                            shrinkWrap: true,
+                            physics: NeverScrollableScrollPhysics(),
+                            itemCount: training.equipments.length,
+                            itemBuilder: (context, index) {
+                              final equipment = training.equipments[index];
+                              return ListTile(
+                                title: Row(
+                                  children: [
+                                    VizorFrame(
+                                      child: Container(
+                                        height: 50,
+                                        child: Image.asset(
+                                          _equipImageList[Random().nextInt(3)],
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
+                                    Text(" " + equipment.name)
+                                  ],
+                                ),
+                                subtitle: ListView.builder(
+                                  shrinkWrap: true,
+                                  physics: NeverScrollableScrollPhysics(),
+                                  itemCount: equipment.records?.length,
+                                  itemBuilder: (context, index) {
+                                    final record = equipment.records?[index];
+                                    return ListTile(
+                                      title: Text("- " +
+                                          (record?.weight.toString() ?? '') +
+                                          " x " +
+                                          (record?.reps.toString() ?? '') +
+                                          " x " +
+                                          (record?.sets.toString() ?? '')),
+                                    );
+                                  },
+                                ),
+                              );
+                            },
+                          ),
+                          Align(
+                            alignment: Alignment.bottomRight,
+                            child: Text(
+                              "weight x reps x sets",
+                              style: TextStyle(
+                                color: Theme.of(context).focusColor,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )),
+            );
+          }
+        },
+        childCount: _items.length + 1,
+      ),
+    );
+  }
+
+  Widget _buildProgressIndicator() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: Opacity(
+          opacity: _isLoading ? 1.0 : 0.0,
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/exercise_page/exercise_page.dart
+++ lib/pages/exercise_page/exercise_page.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:workout_app/api/training_record_api.dart';
 import 'package:workout_app/dialog/choose_equip_dialog.dart';
-import 'package:workout_app/list/exercise_list.dart';
+import 'package:workout_app/list/exercise_list_sliver.dart';
 import 'package:workout_app/models/equip_summary_model.dart';
 import 'package:workout_app/utils/toast_helper.dart';
 import '../../models/equip_model.dart';
@@ -49,123 +49,8 @@ class _ExercisePageState extends State<ExercisePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: 200.0,
-            child: Form(
-              key: _formKey,
-              child: Padding(
-                padding: EdgeInsets.all(20.0),
-                child: Column(
-                  children: <Widget>[
-                    RichText(
-                      text: TextSpan(text: selectedEquip.equip.name),
-                      selectionRegistrar: SelectionContainer.maybeOf(context),
-                      selectionColor: const Color(0xAF6694e8),
-                    ),
-                    ElevatedButton(
-                      style: raisedButtonStyle,
-                      onPressed: () {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return ChooseEquipDialog();
-                          },
-                        ).then((result) {
-                          setState(() {
-                            selectedEquip = result as EquipSummaryModel;
-                          });
-                        });
-                      },
-                      child: Text('Equip select'),
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      children: <Widget>[
-                        Expanded(
-                          child: TextFormField(
-                            controller: weightController,
-                            keyboardType:
-                                TextInputType.numberWithOptions(decimal: true),
-                            // Show decimal keyboard on mobile devices
-                            inputFormatters: [
-                              FilteringTextInputFormatter.allow(
-                                  RegExp(r'^\d+\.?\d{0,2}')),
-                              // Allow only numbers and dots up to 2 decimal places
-                            ],
-                            decoration: InputDecoration(
-                              labelText: 'Weight',
-                            ),
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Please enter weight';
-                              }
-
-                              return null;
-                            },
-                          ),
-                        ),
-                        Expanded(
-                          child: TextFormField(
-                            controller: repsController,
-                            keyboardType:
-                                TextInputType.numberWithOptions(decimal: true),
-                            inputFormatters: [
-                              FilteringTextInputFormatter.allow(
-                                  RegExp(r'^\d+\.?\d{0,2}')),
-                              // Allow only numbers and dots up to 2 decimal places
-                            ], // Show decimal keyboard on mobile devices
-                            decoration: InputDecoration(
-                              labelText: 'reps',
-                            ),
-                            validator: (value) {
-                              if (value == null || value.isEmpty) {
-                                return 'Please enter reps';
-                              }
-
-                              return null;
-                            },
-                          ),
-                        ),
-                      ],
-                    ),
-                    ElevatedButton(
-                      style: raisedButtonStyle,
-                      onPressed: () {
-                        if (_formKey.currentState!.validate()) {
-                          if (selectedEquip.equip.id == 0) {
-                            ToastHelper.fail("Please select equipment");
-                            return;
-                          }
-
-                          TrainingRecordApi.store(
-                            selectedEquip.equip.id,
-                            double.tryParse(weightController.text) ?? 0,
-                            double.tryParse(repsController.text) ?? 0,
-                            ''
-                          ).then((value) {
-                            ToastHelper.success('success');
-                          }).catchError((error) {
-                            ToastHelper.fail('fail');
-                          });
-                        }
-                      },
-                      child: const Text('Add record'),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-          // 下半部份的運動紀錄分頁資料
-          Expanded(
-            child: Scaffold(
-              body: ExerciseList(),
-            )
-          ),
-        ],
-      ),
-    );
+        body: CustomScrollView(
+      slivers: [ExerciseListSliver()],
+    ));
   }
 }
