e8a8a6445d052fba08aa5ee3b886052f2eb1720f
tinghaolai <analth355@gmail.com>
2023-05-28 17:07:01 +0800

    Revert test code

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -16,7 +16,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int currentPage = 1;
+  int currentPage = 0;
   List<Widget> pages = const <Widget>[
     ExercisePage(),
     TrainingRecordPage(),
